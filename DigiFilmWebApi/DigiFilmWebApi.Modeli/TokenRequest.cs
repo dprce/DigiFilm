8cb06c7b30cb050fdb28cb7ad67ca32301b81de0
lKolacevic <luka.kolacevic@fer.unizg.hr>
2024-11-13 18:57:25 +0100

    premjestio token request u zasebni file

---
+++ DigiFilmWebApi/DigiFilmWebApi.Modeli/TokenRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DigiFilmWebApi.Modeli
+{
+    public class TokenRequest
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
