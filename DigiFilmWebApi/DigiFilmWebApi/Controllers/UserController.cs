8b5b206c59524921090c86217d674b0bb4c1d9fd
lukakolacevic <luka.kolacevic@fer.unizg.hr>
2025-01-13 13:05:28 +0100

    Create UserController.cs

---
+++ DigiFilmWebApi/DigiFilmWebApi/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+using DigiFilmWebApi.BAL;
+using DigiFilmWebApi.Modeli;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiFilmWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly IConfiguration _configuration;
+        //private readonly UserRepositoryInterface _userRepositoryInterface;
+        //private readonly PasswordService _passwordService;
+        public UserController(UserService userService, IConfiguration configuration)
+        {
+            _userService = userService;
+            _configuration = configuration;
+        }
+
+        [HttpGet("all-users")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                // Return JSON response for unauthorized access
+                return Unauthorized(new
+                {
+                    Error = "User is not authenticated."
+                });
+            }
+            
+            var users = await _userService.GetAllUsersAsync();
+
+            return Ok(new {users = users, message = "Korisnici uspješno dohvaćeni." });
+        }
+    }
+}
