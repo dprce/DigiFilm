c018730e0d80f619f0508899195cde45aa0dbb8a
lukakolacevic <luka.kolacevic@fer.unizg.hr>
2025-01-13 13:05:34 +0100

    Create BatchListDAO.cs

---
+++ DigiFilmWebApi/DigiFilmWebApi.Modeli/BatchListDAO.cs
@@ -0,0 +1,10 @@
+namespace DigiFilmWebApi.Modeli;
+
+public class BatchListDAO
+{
+    public int BatchID { get; set; }              
+    public string Movies { get; set; }           
+    public string TotalDuration { get; set; }    
+    public string Status { get; set; }  
+    public string PerformedBy { get; set; }
+}
