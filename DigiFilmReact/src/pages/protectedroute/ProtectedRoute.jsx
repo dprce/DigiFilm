b21ec2f30fb702bd76d329114d93a00f89cc45c8
lukakolacevic <luka.kolacevic@fer.unizg.hr>
2025-01-24 22:52:38 +0100

    Update ProtectedRoute.jsx

--- DigiFilmReact/src/pages/protectedroute/ProtectedRoute.jsx
+++ DigiFilmReact/src/pages/protectedroute/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "./auth.js";
+import { isAuthenticated } from "../../auth.js";
 
 const ProtectedRoute = ({ element: Component }) => {
     return isAuthenticated() ? <Component /> : <Navigate to="/" />;
